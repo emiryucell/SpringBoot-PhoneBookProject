239bd10ca4ecb4477129e646951879a448ae76ca
emiryucell <e.talhayucel@gmail.com>
2022-11-17 17:11:00 +0300

    Project Beginning

---
+++ src/main/java/com/emirtalhayucel/solviacase/SolviaCaseApplication.java
@@ -0,0 +1,13 @@
+package com.emirtalhayucel.solviacase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolviaCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolviaCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emirtalhayucel/solviacase/SolviaCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirtalhayucel.solviacase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolviaCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
